8f3bca1befc33c9b1a09778d8c479b82c96dbcc3
Pasta <Tmartins2008@icloud.com>
2024-04-10 14:09:54 -0400

    Finished

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,80 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(800, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  public void draw() {
+
+    // draw all the coloured stickmen
+    stroke(242, 0, 5);
+    manRow(0);
+    stroke(230, 142, 28);
+    manRow(50);
+    stroke(221, 234, 27);
+    manRow(100);
+    stroke(0, 139, 0);
+    manRow(150);
+    stroke(87, 150, 171);
+    manRow(200);
+    stroke(0, 0, 226);
+    manRow(250);
+    stroke(168, 158, 209);
+    manRow(300);
+
+    // draw the hearts
+    for (int y = 1; y < 7; y++) {
+      for (int x = 50; x < 700; x += 100) {
+        drawHeart(x + y % 2 * 50, y * 50);
+      }
+    }
+    
+  }
+
+  private void manRow(int y) {
+
+    // this draws a row, allowing me to adjust the y of manRow() in order to change colours
+    for(int x = 0; x < 750; x += 50){
+      drawMan(x, y);
+    }
+
   }
   
-  // define other methods down here.
+  private void drawMan(int x, int y) {
+
+    fill(210, 255, 173);
+
+    // body
+    line(50 + x, 40 + y, 50 + x, 55 + y);
+    
+    // legs
+    line(50 + x, 55 + y, 60 + x, 65 + y);  
+    line(50 + x, 55 + y, 40 + x, 65 + y);  
+    
+    // head
+    ellipse(50 + x, 30 + y, 20, 20);
+
+    // arms
+    line(50 + x, 50 + y, 60 + x, 40 + y);  
+    line(50 + x, 50 + y, 40 + x, 40 + y);
+
+  }
+
+  private void drawHeart(int x, int y) {
+
+    stroke(0);
+    fill(0);
+    
+    // draw 3 triangles which are adjusted by x and y variables
+    triangle(17 + x, 10 + y, 25 + x, 20 + y, 33 + x, 10 + y);
+    triangle(17 + x, 10 + y, 21 + x, 5 + y, 25 + x, 10 + y);
+    triangle(25 + x, 10 + y, 29 + x, 5 + y, 33 + x, 10 + y);
+
+  }
 }
